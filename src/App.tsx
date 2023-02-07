3373c4a7b6687a7b187687aefb9225204a78928d
Saba Zikaria <szikaria961@gmail.com>
2023-02-07 15:38:54 -0500

    Add cardsize and paper options to autocomplete from mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import { useState, FC, ChangeEvent } from "react";
+import { useState, FC } from "react";
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { dim } from "./constants";
 
 const App: FC = () => {
   const [size, setSize] = useState<string>("");
 
   const handleSubmit = (): void => {
-    alert(size);
-  }
-  const handleSizeChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    const { value } = event.target;
-    setSize(value);
+    const currentPaperSize = dim.filter((dim)=> {
+      return dim.cardSize === size;
+    });
+
+    alert(currentPaperSize[0].paperSize);
+    setSize("");
   }
 
   return (
     <div className="App">
+      <div>{`Size: '${size}'`}</div>
       <div className="header">
-        <input type="text" placeholder="Pick a size.." value={size} onChange={handleSizeChange}/>
+        <Autocomplete
+          getOptionLabel={(option) => option.cardSize}
+          options={dim}
+          sx={{ width: 300 }}
+          inputValue={size}
+          onInputChange={(event, newSize) => {
+            setSize(newSize);
+          }}
+          renderInput={(params) => <TextField {...params} label="cardSize" />}
+        />
         <button onClick = {handleSubmit}>Submit</button>
       </div>
       <div className="returnedSize"></div>
